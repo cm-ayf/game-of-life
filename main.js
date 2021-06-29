16aad3bf097225095e0a7a8b20f7c76758394cae
CallMe AsYouFeel <cm.ayf2734@gmail.com>
2021-06-29 16:07:32 +0900

    table generation

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
+const row = 20;
+const col = 10
 document.addEventListener('DOMContentLoaded', () => {
-    const h1 = document.getElementsByTagName('h1')[0];
-    h1.append(`Conway's Game of Life`);
+    const table = $('table');
+    let tr;
+    for (let i = 0; i < row; i++) {
+        tr = $(`<tr></tr>`).appendTo(table);
+        for (let j = 0; j < col; j++) {
+            $(`<td id="${i},${j}"></td>`).appendTo(tr);
+        };
+    };
 });
