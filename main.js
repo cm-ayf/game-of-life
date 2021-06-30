7e8725b7a57319b130d3c10c53c305d77e5cb9ec
CallMe AsYouFeel <cm.ayf2734@gmail.com>
2021-06-30 12:06:56 +0900

    implemented state

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
 const row = 20;
 const col = 10
+const tableJq = $('table');
+const tableLt = [];
 document.addEventListener('DOMContentLoaded', () => {
-    const table = $('table');
     let tr;
     for (let i = 0; i < row; i++) {
-        tr = $(`<tr></tr>`).appendTo(table);
+        tr = $(`<tr></tr>`).appendTo(tableJq);
+        tableLt[i] = [];
         for (let j = 0; j < col; j++) {
-            $(`<td id="${i},${j}"></td>`).appendTo(tr);
+            tableLt[i][j] = $(`<td id="${i},${j}"></td>`).appendTo(tr);
         };
     };
+
+    tableLt[4][5].addClass('on');
+    tableLt[5][5].addClass('on');
+    tableLt[5][6].addClass('on');
+    tableLt[6][4].addClass('on');
+    tableLt[6][6].addClass('on');
 });
